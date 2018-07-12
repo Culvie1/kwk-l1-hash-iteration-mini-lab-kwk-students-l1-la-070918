d00413c6123cc330e27293ce76acd41706cf95fe
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-12 16:16:09 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,16 +1,40 @@
-
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+def create_olympics_hash 
+  olympics_hash = {
+  :Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"
+}
 end
+
+olympics_hash = {}
+
+index = 0 
+
+olympics_hash.each do |location|
+  olympics_hash[location] = shopping_hash[index] 
+  index = 0 
+
+
+olympics_hash.each do |year, location|
+  puts "The #{year} summer olympics took place in #{location}."
+
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+olympics_hash = {}
+olympics_hash["Sydney"] = "2000"
+olympics_hash["Athens"] = "2004"
+olympics_hash["Bejing"] = "2008"
+olympics_hash["London"] = "2012"
+puts olympics_hash
 end
+puts add_a_key_value_pair
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
+olympics_hash.each do |year|
+  olympics_hash[year] = 
+  # puts "The #{olympics_hash.each do |year| summer olympics took place in #{olympics_hash.each do |location|" 
+end 
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
